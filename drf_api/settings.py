f7446a32e6d54d5f9d6054a79b15cb0d875bfe22
pranjal <pranjaldeepkanel@gmail.com>
2020-10-31 15:27:49 +0545

    Addition of staticfiles and whitenoise middleware.

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = '&(0o2*1iflp8z^c=xjhdw0c#@-uc+eqmu@*=+lyg$=k5#!6y6w'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['summer-api-test.herokuapp.com/','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['summer-api-test.herokuapp.com','127.0.0.1','localhost']
 
 
 # Application definition
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -119,4 +121,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
